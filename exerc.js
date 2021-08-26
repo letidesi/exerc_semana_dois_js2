c14cc65c5441788cffa946713a38337ccc5c874c
Leticia Desiderio <letidesiderio18@gmail.com>
2021-08-26 10:45:20 -0300

    Insere exercíco da semana 2

---
+++ exerc.js
@@ -0,0 +1,23 @@
+alert(' Bem-vindas ao site da Letícia Desiderio.');
+
+let num1= 87;
+let num2= 96;
+let soma = num1 + num2;
+alert('O resultado da soma entre 87 + 96 = ' +soma);
+
+let num3= 5;
+let num4= 8;
+let mult = num3 * num4;
+alert('O resultado da multiplicação entre 5 x 8 = ' +mult);
+
+let listaCompras = ['Arroz', ' Feijão', ' 1 kg de Acém' , ' Beterraba e Alface.'];
+alert('Lista de Compras: ' +listaCompras);
+
+// Treinando aula de Quarta
+
+var nome = prompt('Insira seu nome: ');
+var numero1 = Number(prompt('Insira o primeiro número: '));
+var numero2 = Number(prompt('Insira o seu segundo número:'));
+var conta = numero1 / numero2;
+alert('Olá ' +nome+ ', o resultado da divisão da sua conta é: ' +conta);
+
